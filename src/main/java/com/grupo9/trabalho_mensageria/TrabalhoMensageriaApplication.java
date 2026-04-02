1bef74410142df7df6be7c85b3732142d9259a4d
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2026-04-02 19:27:54 -0300

    first commit

---
+++ src/main/java/com/grupo9/trabalho_mensageria/TrabalhoMensageriaApplication.java
@@ -0,0 +1,13 @@
+package com.grupo9.trabalho_mensageria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoMensageriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoMensageriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo9/trabalho_mensageria/TrabalhoMensageriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo9.trabalho_mensageria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoMensageriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
