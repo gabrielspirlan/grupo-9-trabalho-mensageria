453a65265afe4b7b76d9779de7cf7d0655a22c1b
Henrique Almeida Florentino <henriquealmeidaflorentino61@gmail.com>
2026-04-09 20:11:50 -0300

    OrderRepository.java

---
+++ src/main/java/com/grupo9/trabalho_mensageria/infrastructure/repositories/OrderRepository.java
@@ -0,0 +1,29 @@
+package com.grupo9.trabalho_mensageria.infrastructure.repositories;
+
+import com.grupo9.trabalho_mensageria.domain.entities.Order;
+import com.grupo9.trabalho_mensageria.domain.enums.OrderStatusEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.UUID;
+
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+
+    @Query(
+            "SELECT DISTINCT o FROM orders o " +
+            "LEFT JOIN FETCH o.items i " +
+            "WHERE (:clientId IS NULL OR o.customer.id = :clientId) " +
+            "AND (:status IS NULL OR o.status = :status) " +
+            "AND (:productId IS NULL OR i.product.id = :productId)"
+    )
+    Page<Order> findWithFilters(
+        @Param("clientId") Integer clientId,
+        @Param("productId") Integer productId,
+        @Param("status") OrderStatusEnum status,
+        Pageable pageable
+    );
+}
