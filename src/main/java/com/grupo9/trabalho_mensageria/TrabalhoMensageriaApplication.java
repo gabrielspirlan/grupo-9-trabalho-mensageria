b4b38170221006c6eb743a022ba21b06305cc855
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2026-04-09 21:27:17 -0300

    TrabalhoMensageriaApplication.java

--- src/main/java/com/grupo9/trabalho_mensageria/TrabalhoMensageriaApplication.java
+++ src/main/java/com/grupo9/trabalho_mensageria/TrabalhoMensageriaApplication.java
@@ -2,8 +2,9 @@ package com.grupo9.trabalho_mensageria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class TrabalhoMensageriaApplication {
 
 	public static void main(String[] args) {
