296a31e53a344cbbcf33a9cc3a10b3061130cef8
ITzspi <felipeavelinopedaes@gmail.com>
2026-04-11 07:30:08 -0300

    SubscribeAsync.java

---
+++ src/main/java/com/grupo9/trabalho_mensageria/mensageria/SubscribeAsync.java
@@ -0,0 +1,53 @@
+package com.grupo9.trabalho_mensageria.mensageria;
+
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.MessageReceiver;
+import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.pubsub.v1.ProjectSubscriptionName;
+import com.google.pubsub.v1.PubsubMessage;
+import com.grupo9.trabalho_mensageria.application.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Component
+public class SubscribeAsync implements CommandLineRunner {
+
+    @Autowired
+    private MessageService messageService;
+
+    public void run(String... args) throws Exception {
+        String projectId = "serjava-demo";
+        String subscriptionId = "sub-grupo9";
+
+        subscribeAsync(projectId, subscriptionId);
+    }
+
+    public void subscribeAsync(String projectId, String subscriptionId) {
+        ProjectSubscriptionName subscriptionName = ProjectSubscriptionName.of(projectId, subscriptionId);
+
+        MessageReceiver receiver = (PubsubMessage message, AckReplyConsumer consumer) -> {
+            String data = message.getData().toStringUtf8();
+             if (messageService.processMessage(data)) {
+//                  consumer.ack();
+             } else {
+//                  consumer.nack();
+             }
+        };
+
+        Subscriber subscriber = null;
+        try {
+            subscriber = Subscriber.newBuilder(subscriptionName, receiver).build();
+            subscriber.startAsync().awaitRunning();
+            System.out.println("Escutando Mensagem: " + subscriptionName.toString());
+            subscriber.awaitTerminated(30, TimeUnit.SECONDS);
+        } catch (TimeoutException timeoutException) {
+            if (subscriber != null) {
+                subscriber.stopAsync();
+            }
+        }
+    }
+}
